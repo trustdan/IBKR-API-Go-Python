e5df6c21f1395a86b9540e6f103980527d5fbc29
trustdan <danieltuckerrust@gmail.com>
2025-05-06 20:49:31 -0500

    Fix Docker build issues by adding Dockerfiles and Kubernetes deployments

--- go/cmd/scanner/imports.go
+++ go/cmd/scanner/imports.go
@@ -6,14 +6,38 @@
 package main
 
 import (
-	// Standard library packages
-	_ "expvar"
-	_ "net/http/httputil"
+	// Standard library imports
+	_ "context"
+	_ "encoding/json"
+	_ "flag"
+	_ "fmt"
+	_ "io"
+	_ "log"
+	_ "net/http"
+	_ "os"
+	_ "os/signal"
+	_ "strings"
+	_ "sync"
+	_ "syscall"
+	_ "time"
 
-	// Third-party packages
+	// Third-party imports
+	_ "github.com/patrickmn/go-cache"
+	_ "github.com/prometheus/client_golang/prometheus"
+	_ "github.com/prometheus/client_golang/prometheus/promhttp"
 	_ "github.com/prometheus/common/expfmt"
+	_ "github.com/shirou/gopsutil/cpu"
+	_ "github.com/shirou/gopsutil/mem"
+	_ "github.com/sirupsen/logrus"
 	_ "golang.org/x/net/http2"
 	_ "golang.org/x/net/trace"
+	_ "google.golang.org/grpc"
+	_ "google.golang.org/protobuf/proto"
+	_ "gopkg.in/yaml.v3"
+
+	// Internal imports - update these paths to match your actual project structure
+	_ "github.com/trustdan/ibkr-trader/go/src/config"
+	_ "github.com/trustdan/ibkr-trader/go/src/metrics"
 )
 
 // This function is never called, it's just here to prevent compiler warnings
